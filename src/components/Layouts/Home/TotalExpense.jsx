409ff6fab8b65eb37312dba51cd60f54f288f990
Andrew2432 <drew.devops@gmail.com>
2020-06-06 17:18:12 +0530

    Add context in TotalExpense

--- src/components/Layouts/Home/TotalExpense.jsx
+++ src/components/Layouts/Home/TotalExpense.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ExpenseContext from '../../../context/Expense/expenseContext';
 import { CardPanel } from 'react-materialize';
 
 const TotalExpense = () => {
+  const expenseContext = useContext(ExpenseContext);
+  const { totalExpense } = expenseContext;
+
   return (
     <CardPanel
       className="deep-orange darken-1 white-text"
       style={{ textAlign: 'center' }}
     >
       <h4>Total expense</h4>
-      <h4>Rs. 1000</h4>
+      <h4>Rs. {totalExpense}</h4>
     </CardPanel>
   );
 };
