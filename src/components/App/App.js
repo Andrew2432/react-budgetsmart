cf20a08e42d2add71c970f47b70265c7e037ecd2
Andrew2432 <drew.devops@gmail.com>
2020-06-06 07:50:28 +0530

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello</h1>
+    </Container>
+  );
+}
+
+export default App;
